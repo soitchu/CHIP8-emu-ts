1755311beab6a3485c3174ed75b9bc6cd73df2b0
soitchu <soitchu@users.noreply.github.com>
2025-01-02 23:21:54 -0800

    feat: add an option to load tetris

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,14 @@ const menuCon = document.getElementById("menuCon")!;
 const menuIcon = document.getElementById("menuIcon")!;
 const display = document.getElementById("display")! as HTMLCanvasElement;
 const container = document.getElementById("container")!;
+const tetrisLoader = document.getElementById("tetrisLoader")!;
 
 const host = new CHIP8Host(display.transferControlToOffscreen());
-new EmuMenu(container, menuCon, menuIcon, display, host);
+const menu = new EmuMenu(container, menuCon, menuIcon, display, host);
+
+tetrisLoader.addEventListener("click", () => {
+  menu.loadROM(
+    "games",
+    menu.programs.games.find((p) => p.toLowerCase().includes("tetris"))!
+  );
+});
